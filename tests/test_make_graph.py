7f4f94f79763a834d84e27fb9c64b7e00f88cb39
kompalan <kompalli@msu.edu>
2025-11-30 03:21:34 +0000

    Reorganized directory structure

--- tests/test_make_graph.py
+++ tests/test_make_graph.py
@@ -1,4 +1,4 @@
-from py2graph.make_graph import make_graph_from_src, Node, FunctionNode
+from ragtag.py2graph.make_graph import make_graph_from_src, Node, FunctionNode
 
 def make_simple_graph():
     graph = make_graph_from_src('./tests/golden/simple_test')
